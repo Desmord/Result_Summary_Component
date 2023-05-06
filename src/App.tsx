592f709e6a2eefd8fe67e4c96d012e8cbe7461cf
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-05-06 18:22:29 +0200

    Deleted old project verison

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Result from './Components/Result/Result';
+import Summary from './Components/Summary/Summary';
+
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      Witam
+      {/* <Result />
+      <Summary /> */}
+    </div>
+  );
+}
+
+export default App;
