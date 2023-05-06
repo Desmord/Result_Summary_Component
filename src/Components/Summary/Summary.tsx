f55c0e679918b4b295db5809d86e15d8ad01503e
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-05-06 17:21:40 +0200

    Add Summary component

--- src/Components/Summary/Summary.tsx
+++ src/Components/Summary/Summary.tsx
@@ -1,8 +1,29 @@
+import { results, TypeResult } from '../../Utilities/data';
+
 import styles from './Summary.module.scss';
 
 const Summary = () => {
     return (
-        <div className={styles.container}>Summary</div>
+        <div className={styles.container}>
+            <div className={styles.title}>Summary</div>
+            <div className={styles.scoreList}>
+                {results.map((result: TypeResult, index: number) => (
+                    <div
+                        className={styles.listElement}
+                        key={index}>
+                        <div className={styles.iconContainer}>
+                            <img src={result.icon} alt={result.category} />
+                        </div>
+                        <div className={styles.resultCategory}>{result.category}</div>
+                        <div className={styles.resultScore}>
+                            <span>{result.score} </span>
+                            <span>&nbsp;/ 100</span>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.continueButton}>Continue</div>
+        </div>
     )
 }
 
